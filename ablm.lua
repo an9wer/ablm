7c9058ca4e51bd1ed6e2784e64ef42ac3b5eed88
Runney Wu <an9wer@gmail.com>
2023-10-18 09:54:48 +1030

    Implement AB-loop adjustment

--- ablm.lua
+++ ablm.lua
@@ -49,11 +49,36 @@ function ABLoop.move(step)
 
     local loop_a, loop_b = ABLoop.parse(ABLoop.list[ABLoop.curr])
 
-    mp.command("ab-loop")
     mp.set_property("ab-loop-a", loop_a)
     mp.set_property("ab-loop-b", loop_b)
     mp.command(string.format("seek %s absolute", loop_a))
     mp.osd_message(string.format("Restore A-B loop: [%d/%d] %s - %s", ABLoop.curr, #ABLoop.list, loop_a, loop_b))
+end
+
+function ABLoop.adjust(a_or_b, step)
+    local loop_a = tonumber(mp.get_property("ab-loop-a"))
+    local loop_b = tonumber(mp.get_property("ab-loop-b"))
+
+    if loop_a == nil or loop_b == nil then
+        mp.osd_message("No A-B loop to adjust")
+        return
+    end
+
+    if a_or_b == "a" then
+        loop_a = loop_a + step
+    end
+    if a_or_b == "b" then
+        loop_b = loop_b + step
+    end
+
+    if loop_a > loop_b then
+        loop_a, loop_b = loop_b, loop_a
+    end
+
+    mp.set_property("ab-loop-a", loop_a)
+    mp.set_property("ab-loop-b", loop_b)
+    mp.command(string.format("seek %s absolute", loop_b - 0.5))
+    mp.osd_message(string.format("Adjust A-B loop: %s - %s", loop_a, loop_b))
 end
 
 function ABLoop.save()
@@ -64,13 +89,11 @@ function ABLoop.save()
     if loop_a == "no" or loop_b == "no" then
         mp.osd_message("No A-B loop to save")
     else
-        mp.osd_message(string.format("Save A-B loop: %s - %s", loop_a, loop_b))
-
         -- TODO: catching errors
         local f, err, errno = io.open(path .. ".abl", "a+")
-
         f:write(string.format("%s,%s\n", loop_a, loop_b))
         f:close()
+        mp.osd_message(string.format("Save A-B loop: %s - %s", loop_a, loop_b))
     end
 end
 
@@ -79,3 +102,7 @@ mp.add_hook("on_preloaded", 50, ABLoop.load)
 mp.add_key_binding("Alt+l", "ab-loop-save", ABLoop.save)
 mp.add_key_binding("Ctrl+l", "ab-loop-next", function () ABLoop.move(1) end)
 mp.add_key_binding("Ctrl+L", "ab-loop-prev", function () ABLoop.move(-1) end)
+mp.add_key_binding("Alt+LEFT", "ab-loop-fwd-b", function () ABLoop.adjust("b", -0.2) end)
+mp.add_key_binding("Alt+RIGHT", "ab-loop-bwd-b", function () ABLoop.adjust("b", 0.2) end)
+mp.add_key_binding("Ctrl+LEFT", "ab-loop-fwd-a", function () ABLoop.adjust("a", -0.2) end)
+mp.add_key_binding("Ctrl+RIGHT", "ab-loop-bwd-a", function () ABLoop.adjust("a", 0.2) end)
